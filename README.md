a88bcc1dc5ff392d7f0a28539f16037beeb83802
Mohammad Minhas Mustafa <your.email@example.com>
2026-02-16 09:12:21 +0500

    Updated fine-tuned model notebook, gradio app, and README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Custom LLM Chatbot
 
-Mini-project: Fine-tuned distillgpt2 on personal notes Q&A for a custom chatbot. Built with Gradio, deployed on Hugging Face Spaces.
+Mini-project: Fine-tuned GPT-2 on personal notes Q&A for a custom chatbot. Built with Gradio, deployed on Hugging Face Spaces.
 
 # Features
 
--- gradio_app.py
+++ gradio_app.py
@@ -3,36 +3,37 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 from peft import PeftModel
 
-base_model = "distilgpt2"                                                       # Base from HF
-Hf_repo = "MohammadMinhasMustafa/distilgpt2_fine_tuned_for_custom_ChatBOT"      # HF repo
+base_model = "openai-community/gpt2"                                                # Base from HF
+Hf_repo = "MohammadMinhasMustafa/GPT2_fine_tuned_for_custom_ChatBOT"                # HF repo
 
 tokenizer = AutoTokenizer.from_pretrained(base_model)
 model = AutoModelForCausalLM.from_pretrained(base_model)
-model = PeftModel.from_pretrained(model, Hf_repo)                               # Add adapters
+model = PeftModel.from_pretrained(model, Hf_repo)                                   # Add adapters
 
 def chatbot(prompt):
     inputs = tokenizer(prompt, return_tensors = 'pt')
-    with torch.no_grad():               # Saves Memory and Speed
+    with torch.no_grad():                                                           # Saves Memory and Speed
         outputs = model.generate(
             inputs['input_ids'], 
             max_length = 150,
-            temperature = 0.7,          # Balanced creativity
-            do_sample = True,           # For variety
+            temperature = 0.7,                                                      # Balanced creativity
+            do_sample = True,                                                       # For variety
             top_k = 40,
-            top_p = 5,                  # Focus on likely words
-            repetition_penalty = 1.2    # Less repetition
+            top_p = 5,                                                              # Focus on likely words
+            repetition_penalty = 1.2                                                # Less repetition
             )
     generated_text = tokenizer.decode(outputs[0])
     return generated_text[len(prompt):].strip()  # Trim prompt, remove extras
 
 gr.Interface(fn=chatbot, 
-             inputs=gr.Textbox(label="Ask Anything", placeholder="Type your question here..."), # Nicer input with label/placeholder
+             inputs=gr.Textbox(label="Ask Anything", placeholder="Type your question here..."),       # Nicer input with label/placeholder
              outputs=gr.Textbox(label="Response"), 
              title="My Custom Chatbot", 
-             description="Chat with AI tuned on my notes! Ask generic QnAs",  # Intro text"
-             theme = gr.themes.Soft(primary_hue = "blue", secondary_hue="gray"),  # Attractive theme
-             examples=[["What is your favorite color and why?"], ["What is the capital city of Pakistan?"]],  # Quick-click examples
-             flagging_mode = "never",    # Hide flag button
-             preload_example = False     # his prevents auto-filling the input on load.
+             description="Chat with AI tuned on my notes! Ask generic QnAs",        # Intro text"
+             theme = gr.themes.Soft(primary_hue = "blue", secondary_hue="gray"),    # Attractive theme
+             examples=[["What is your favorite color and why?"], 
+             ["What is the capital city of Pakistan?"]],                            # Quick-click examples
+             flagging_mode = "never",                                               # Hide flag button
+             preload_example = False                                                # his prevents auto-filling the input on load.
              
              ).launch(share=True)
